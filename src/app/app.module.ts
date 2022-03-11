76b9bd480de31cf3677abfcf1fe3c49545c284dd
Timo Gross <gross.timo95@gmail.com>
2022-03-11 22:46:56 +0100

    GameSettingForm finished

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,18 +2,32 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+
 
 // Material Modules
 import {MatToolbarModule} from '@angular/material/toolbar';
 import {MatIconModule} from '@angular/material/icon';
 import {MatButtonModule} from '@angular/material/button';
 import {MatSidenavModule} from '@angular/material/sidenav';
+import {MatCheckboxModule} from '@angular/material/checkbox';
+import {MatButtonToggleModule} from '@angular/material/button-toggle';
+import {MatInputModule} from '@angular/material/input';
+import {MatFormFieldModule} from '@angular/material/form-field';
+import {MatSelectModule} from '@angular/material/select';
+import {MatDividerModule} from '@angular/material/divider';
+
+
+
+
+
+
 
 // own Components
-import { FirstToZeroComponent } from './components/games/first-to-zero/first-to-zero.component';
+import { AppComponent } from './app.component';
 import { DashboardComponent } from './components/dashboard/dashboard.component';
+import { FirstToZeroComponent } from './components/games/first-to-zero/first-to-zero.component';
 
 
 @NgModule({
@@ -23,12 +37,20 @@ import { DashboardComponent } from './components/dashboard/dashboard.component';
     DashboardComponent,
   ],
   imports: [
-    BrowserModule,
     MatToolbarModule,
     MatIconModule,
     MatButtonModule,
     MatSidenavModule,
+    MatCheckboxModule,
+    MatButtonToggleModule,
+    MatInputModule,
+    MatFormFieldModule,
+    MatSelectModule,
+    MatDividerModule,
     AppRoutingModule,
+    FormsModule,
+    ReactiveFormsModule,
+    BrowserModule,
     BrowserAnimationsModule
   ],
   providers: [],
--- src/app/components/games/first-to-zero/first-to-zero.component.ts
+++ src/app/components/games/first-to-zero/first-to-zero.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { FormGroup ,FormArray, FormControl } from '@angular/forms';
 
 @Component({
   selector: 'app-first-to-zero',
@@ -6,10 +7,63 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./first-to-zero.component.scss']
 })
 export class FirstToZeroComponent implements OnInit {
-
-  constructor() { }
+  ruleForm: FormGroup;
+  playersArray: FormArray;
+  //
+  public showCustomScore = false;
+  public settingsFinished = false;
+  // 
+  constructor() { 
+    this.ruleForm = new FormGroup({
+      startScore: new FormControl('501'),
+      customScore: new FormControl('60'),
+      inCondition: new FormControl('1'),
+      outCondition: new FormControl('2'),
+      difference: new FormControl('0'),
+      finish: new FormControl('bestOf'),
+      number: new FormControl('3'),
+      type: new FormControl('legs'),
+      players: new FormArray([
+        new FormGroup({
+          name: new FormControl(''),
+          team: new FormControl(0),
+          bot: new FormControl('')
+        })
+      ]),
+    })
+    
+    this.playersArray = <FormArray>this.ruleForm.controls['players'];
+  
+   }
 
   ngOnInit(): void {
+  }
+  onChangeScore(val : string){
+    if (val === "custom"){
+      this.showCustomScore = true;
+    }
+    else{
+      this.showCustomScore = false;
+      this.ruleForm.patchValue({startScore: val})
+    }
+  }
+  private createPlayerControl(count: number): FormGroup{
+    return new FormGroup({
+      name:new FormControl(''),
+      team: new FormControl(count),
+      bot: new FormControl('')
+    })
+  }
+  addPlayer(count: number){
+    this.playersArray.push(this.createPlayerControl(count))
+  }
+  removePlayer(i: number){
+    this.playersArray.removeAt(i);
+    return false;
+  }
+  startGame(){
+    this.settingsFinished = true;
+    console.log(this.ruleForm.value)
   }
 
 }
